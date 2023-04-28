614510c0e284f0389d6211271fc30d9e7ab35d9b
z0wade <82123241+z0wade@users.noreply.github.com>
2023-04-28 16:20:00 -0400

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -29,6 +29,27 @@ class Data:
         self.times = np.array([datetime[i][dateIndex+1:timeIndex] for i in range(len(datetime))])
         #self.datetime = np.array(datetime)
         self.datetime = np.array([datetime[i].replace('T', ' ')[:-1] for i in range(len(datetime))])
+        
+    def filterData(self, start_date=None, end_date=None):
+        if start_date is None:
+            start_date = input("Enter start date (YYYY-MM-DD): ")
+        if end_date is None:
+            end_date = input("Enter end date (YYYY-MM-DD): ")
+            
+        #convert start_date and end_date strings to datetime objects
+        start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d")
+        end_date = datetime.datetime.strptime(end_date, "%Y-%m-%d")
+        
+        #create condition to filter the summary dataframe based on user input
+        condition = (self.dates >= start_date.strftime("%Y-%m-%d")) & (self.dates <= end_date.strftime("%Y-%m-%d"))
+        
+        #create new instance of data class for filtered data
+        filtered_summary = self.summary[condition]
+        filtered_data = Data("")
+        filtered_data.summary = filtered_summary
+        filtered_data.meta = self.meta
+        filtered_data.preprocessTime()
+        return filtered_data
 
 class DataLoader:
     #function: adds data stream to list
